11254ddb8b8a0b66b57404c932e3c81ce1b39053
ash2d <danielmartinash@gmail.com>
2025-06-18 11:55:16 +0100

    relatednesssss

--- exploring/evaluate.sh
+++ exploring/evaluate.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=train_mgd
 #SBATCH --output=logs/evaluate_%j/o.out
 #SBATCH --error=logs/evaluate_%j/e.err
-#SBATCH --time=1:00:00
+#SBATCH --time=10:00:00
 #SBATCH --partition=orchid
 #SBATCH --account=orchid
 #SBATCH --qos=orchid
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=40G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -23,4 +23,4 @@ source /home/users/dash/guppies/embeddings/wildlife-tools/.venv/bin/activate
 cd exploring
 
 # Run your Python script with desired arguments
-srun  uv run evaluate_reid.py --model "hf-hub:BVRA/MegaDescriptor-T-224" --save-path '/gws/nopw/j04/iecdt/dash/embeddings/models/MGD/MGD_30000ids_arcface/model_epoch_9.pt/checkpoint.pth' --database /home/users/dash/guppies/embeddings/wildlife-tools/exploring/csvs/top_1000_individuals_database.csv --query /home/users/dash/guppies/embeddings/wildlife-tools/exploring/csvs/top_1000_individuals_query.csv --root '/gws/nopw/j04/iecdt/dash/cropped_images/id/w_random_masks' --output '1000ids_results.txt'
+srun  uv run evaluate_reid.py --model "hf-hub:BVRA/MegaDescriptor-T-224" --save-path '/gws/nopw/j04/iecdt/dash/embeddings/models/MGD/MGD_30000ids_arcface/model_epoch_10.pt/checkpoint.pth' --database /home/users/dash/guppies/embeddings/wildlife-tools/exploring/csvs/top_30000_individuals_database.csv --query /home/users/dash/guppies/embeddings/wildlife-tools/exploring/csvs/top_30000_individuals_query.csv --root '/gws/nopw/j04/iecdt/dash/cropped_images/id/w_random_masks' --output '30000ids_results.txt'
